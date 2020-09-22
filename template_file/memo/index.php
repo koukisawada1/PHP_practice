8378ed673d08bd335277b53521daacb6f563d4a4
澤田こうき <63055099+koukisawada1@users.noreply.github.com>
2020-09-22 20:59:57 +0900

    ページネーション機能作成

--- template_file/memo/index.php
+++ template_file/memo/index.php
@@ -21,8 +21,19 @@
 // require 他のファイルを取り込むことができるファンクション
 require('dbconnect.php');
 
+// ページネーション
+// urパラメータを省略した場合,数字が指定されなかった場合はpage=1が表示される
+if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) {
+   $page = $_REQUEST['page']; 
+} else {
+    $page = 1;
+}
 
-$memos = $db->query('SELECT * FROM memos ORDER BY id DESC');
+$start = 5 * ($page - 1);
+
+$memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC LIMIT ?, 5');
+$memos->bindParam(1, $start, PDO::PARAM_INT); // 数字でパラメータを渡す
+$memos->execute();
 
 ?>
 
@@ -33,6 +44,22 @@ $memos = $db->query('SELECT * FROM memos ORDER BY id DESC');
         <time><?php print($memo['created_at']); ?></time>
         <hr>
     <?php endwhile; ?>
+    <!-- 0ページへを非表示にする -->
+    <?php if ($page >= 2): ?>
+        <a href="index.php?page=<?php print($page-1); ?>"><?php print($page-1); ?>ページ目へ</a>
+    <?php endif ?>
+    ｜
+    <?php
+    // データがないページへのリンクを非表示にする
+    // データの件数を取得する　as cnt $countをcntで使えるようにする
+    $counts = $db->query('SELECT COUNT(*) as cnt FROM memos');
+    $count = $counts->fetch();
+    // ceil 切り上げる　マックスページを求める計算
+    $max_page = ceil($count['cnt'] / 5);
+    if ($page < $max_page):
+    ?>
+        <a href="index.php?page=<?php print($page+1); ?>"><?php print($page+1); ?>ページ目へ</a>
+    <?php endif; ?>
 </article>
 </main>
 </body>    
