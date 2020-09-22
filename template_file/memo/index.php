a12d5b370f9b088b3a192af3578bbfcc8b59deb7
澤田こうき <63055099+koukisawada1@users.noreply.github.com>
2020-09-22 17:41:29 +0900

    query SELECT SQを実行する

--- template_file/memo/index.php
+++ template_file/memo/index.php
@@ -27,10 +27,20 @@ try {
     $db = new PDO('mysql:dbname=mydb;127.0.0.1;charset=utf8', 'root', 'root');   
 } catch(PDOException $e) {
     echo 'DB接続エラー' . $e->getMessage();
-} 
+}
+ 
 //exec データベースにSQLを発行する
-$count = $db -> exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰,ピンク,甘い"');
-echo $count . '件のデータを挿入しました';
+// $count = $db -> exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰,ピンク,甘い"');
+// echo $count . '件のデータを挿入しました';
+
+//$recodesのなかにデータベースの内容を入れる
+// queryメゾット　SELECT構文で得られた結果を取得する my_itemsから全てのカラムの値を取得してください
+$recodes = $db->query('SELECT * FROM my_items');
+// fetch 一行ずつ取り出す
+while ($recode = $recodes->fetch()) {
+    print($recode['item_name'] . "\n");
+}
+
 ?>
 </pre>
 </main>
